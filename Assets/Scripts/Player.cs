454d5dc7ae4366171ab30badabc41cfb91012ce6
NikoMyoji <nicolas.vlte@gmail.com>
2023-02-14 10:21:16 +0100

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private int ScoreValue = 0;
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private ScenarioData _scenario;
+    [SerializeField] private GameObject _wallPrefab;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _scoreText.text = "Score : "+ScoreValue;
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) 
+        {
+            _rigidbody.AddForce(Input.GetAxis("Horizontal") * 0.5f, 0f, Input.GetAxis("Vertical"));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target_Trigger"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+<<<<<<< Updated upstream
+=======
+    private void UpdateScore()
+    {
+        ScoreValue++;
+        PlayerPrefs.SetString("Score", "Score : " + ScoreValue.ToString());
+        _scoreText.text = PlayerPrefs.GetString("Score");
+        Instantiate(_wallPrefab, _scenario.FirstWalls[0], Quaternion.identity);
+        if(ScoreValue == 8)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+>>>>>>> Stashed changes
+}
