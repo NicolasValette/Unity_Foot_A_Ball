03c2e1ae9e3076fac56970170af376903f48fc39
NikoMyoji <nicolas.vlte@gmail.com>
2023-02-15 17:14:58 +0100

    update lighting

--- Assets/Scripts/Character/CubeTarget.cs
+++ Assets/Scripts/Character/CubeTarget.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CubeTarget : Target
 {
 
-    [SerializeField]
-    private GameObject _playerToFollow;
     // Start is called before the first frame update
     void Start()
     {
