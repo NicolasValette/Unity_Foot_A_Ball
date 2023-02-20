15e386680aef39d8b6559472ea0214c2ea0f85e0
NikoMyoji <nicolas.vlte@gmail.com>
2023-02-20 16:57:12 +0100

    add navmesh

--- Assets/Scripts/Character/TeamMember.cs
+++ Assets/Scripts/Character/TeamMember.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TeamMember : Target
 {
+    [SerializeField]
+    private Transform _goal;
     public static event Action PlayerDead;
     public bool IsTargeted { get; set; }
+    private NavMeshAgent _agent;
 
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = _goal.position;
+    }
     private void OnDestroy()
     {
         if (IsTargeted)
@@ -15,6 +24,10 @@ public class TeamMember : Target
             Debug.Log("Player Dead");
             PlayerDead?.Invoke();
         }
+    }
+    private void Update()
+    {
+        _agent.destination = _goal.position;
     }
 
 }
